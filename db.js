6397345fd3893d9039332045b008f066ba8c111a
Marten Luhalaid <marten.luhalaid@gmail.com>
2026-03-08 23:37:02 +0200

    final assignment code

--- db.js
+++ db.js
@@ -4,7 +4,10 @@ const { Pool } = require('pg');
 
 
 const itemsPool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 module.exports = itemsPool;
--- server.js
+++ server.js
@@ -19,6 +19,8 @@ db.connect((error)  => {
 
 app.use('/todo', todoRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000.');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
